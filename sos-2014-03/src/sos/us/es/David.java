3192d3adbc70d0d342428e5ab20d61531020a2e3
David López Carrascal <davlopcar@users.noreply.github.com>
2014-03-06 15:36:12 +0000

    Servlet David

---
+++ sos-2014-03/src/sos/us/es/David.java
@@ -0,0 +1,11 @@
+package sos.us.es;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class David extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, David");
+	}
+}
