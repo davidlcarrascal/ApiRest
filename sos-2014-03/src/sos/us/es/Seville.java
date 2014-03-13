74ad9aa11e40413af29a9b386daba476ef0f146a
jenguesan <jenguesan@43ee67e5-c0c0-4f48-802a-26a3a79503e4>
2014-03-13 16:31:01 +0000

    servlet jenny

---
+++ sos-2014-03/src/sos/us/es/Seville.java
@@ -0,0 +1,28 @@
+package sos.us.es;
+
+public class Seville {
+
+	Integer population;
+	Integer year;
+	
+	public Seville(Integer population, Integer year) {
+		super();
+		this.population = population;
+		this.year = year;
+	}
+	public Integer getPopulation() {
+		return population;
+	}
+	public void setPopulation(Integer population) {
+		this.population = population;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	
+	
+	
+}
