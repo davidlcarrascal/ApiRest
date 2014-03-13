71de1f96034c85d07c84925d27b033610293ffc9
David López Carrascal <davlopcar@users.noreply.github.com>
2014-03-13 16:02:11 +0000

    asdf

---
+++ sos-2014-03/src/sos/us/es/SpainStd.java
@@ -0,0 +1,21 @@
+package sos.us.es;
+
+public class SpainStd {
+	
+	String name;
+	
+	
+	public SpainStd() {
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+}
