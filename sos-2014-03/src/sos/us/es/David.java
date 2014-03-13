bac99b5a24a9d04f2580c4abd0fb482cb15e1451
David López Carrascal <davlopcar@users.noreply.github.com>
2014-03-13 16:22:03 +0000

    kiu

--- sos-2014-03/src/sos/us/es/David.java
+++ sos-2014-03/src/sos/us/es/David.java
@@ -1,18 +1,33 @@
 package sos.us.es;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.*;
+
+import com.google.gson.Gson;
 
 @SuppressWarnings("serial")
 public class David extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		//String json;
-		
+		String json;
+
 		// Crear datos de prueba- Crear lista de 2 anios
+		Gson gson = new Gson();
+		
+		SpainStd stad1 = new SpainStd("PresupuestoEduc",20000000);
+		SpainStd stad2 = new SpainStd("Poblacion",3000000);
+		
+		List<SpainStd> l =new ArrayList<SpainStd>();
+		l.add(stad1);
+		l.add(stad2);
 		
 		// Serializar a JSON
+		json=gson.toJson(l);
+		
 		
 		resp.setContentType("text/json");
-		//resp.getWriter().println(json);
+		resp.getWriter().println(json);
 	}
 	
 }
--- sos-2014-03/src/sos/us/es/SpainStd.java
+++ sos-2014-03/src/sos/us/es/SpainStd.java
@@ -3,11 +3,16 @@ package sos.us.es;
 public class SpainStd {
 	
 	String name;
+	Integer num;
 	
 	
 	public SpainStd() {
+		super();
 	}
-	
+	public SpainStd(String n, Integer i) {
+		name = n;
+		num=i;
+	}
 	public String getName() {
 		return name;
 	}
