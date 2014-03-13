d21fea380ffc75aa0713577866e5c64e176d39b6
jenguesan <jenguesan@43ee67e5-c0c0-4f48-802a-26a3a79503e4>
2014-03-13 16:31:01 +0000

    servlet jenny

--- trunk/sos-2014-03/src/sos/us/es/Jenny.java
+++ trunk/sos-2014-03/src/sos/us/es/Jenny.java
@@ -1,11 +1,34 @@
 package sos.us.es;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.servlet.http.*;
+
+import com.google.gson.Gson;
 
 @SuppressWarnings("serial")
 public class Jenny extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/html");
-		resp.getWriter().println("Hola Mundo!");
+		
+		String json;
+		Gson gson = new Gson();
+		
+		//Crear datos de prueba : Crear lista de 2 poblaciones
+		
+		Seville s1 = new Seville(20000000, 2008);
+		Seville s2 = new Seville(5000000, 2010);
+		
+		List<Seville> sev = new LinkedList<Seville>();
+		
+		sev.add(s1);
+		sev.add(s2);
+		
+		//Serializar
+		json = gson.toJson(sev);
+		
+		//responder
+		resp.setContentType("text/json");
+		resp.getWriter().println(json);
 	}
 }
