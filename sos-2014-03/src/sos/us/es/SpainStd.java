4ffd7736e7608bc8bf28af429a91ac57c395bf4f
David López Carrascal <davlopcar@users.noreply.github.com>
2014-03-18 18:51:52 +0000

    Modificado

--- sos-2014-03/src/sos/us/es/SpainStd.java
+++ sos-2014-03/src/sos/us/es/SpainStd.java
@@ -2,25 +2,31 @@ package sos.us.es;
 
 public class SpainStd {
 	
-	String name;
-	Integer num;
+	Integer year;
+	Integer population;
 	
 	
 	public SpainStd() {
 		super();
 	}
-	public SpainStd(String n, Integer i) {
-		name = n;
-		num=i;
+	public SpainStd(Integer year, Integer population) {
+		this.year = year;
+		this.population=population;
 	}
-	public String getName() {
-		return name;
+	public Integer getYear() {
+		return year;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setYear(Integer year) {
+		this.year = year;
 	}
-
+	public Integer getPopulation() {
+		return population;
+	}
+	public void setPopulation(Integer population) {
+		this.population = population;
+	}
+	
+	
 
 
 }
