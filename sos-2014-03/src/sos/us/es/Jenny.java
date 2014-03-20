e3d38f5461a250117c7baf3ff89001e526f7d443
jenguesan <jenguesan@43ee67e5-c0c0-4f48-802a-26a3a79503e4>
2014-03-20 08:29:59 +0000

    doget jenny

--- sos-2014-03/src/sos/us/es/Jenny.java
+++ sos-2014-03/src/sos/us/es/Jenny.java
@@ -9,26 +9,30 @@ import com.google.gson.Gson;
 
 @SuppressWarnings("serial")
 public class Jenny extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	
 		
-		String json;
+		List<Seville> sev = new LinkedList<>();
+		//String json;
 		Gson gson = new Gson();
 		
 		//Crear datos de prueba : Crear lista de 2 poblaciones
+		public void init(){
+			Seville s1 = new Seville(2008, 20000000);
+			Seville s2 = new Seville(2010, 5000000);
 		
-		Seville s1 = new Seville(2008, 20000000);
-		Seville s2 = new Seville(2010, 5000000);
-		
-		List<Seville> sev = new LinkedList<Seville>();
-		
-		sev.add(s1);
-		sev.add(s2);
-		
+		//List<Seville> sev = new LinkedList<Seville>();
+			sev.add(s1);
+			sev.add(s2);
+		}
 		//Serializar
-		json = gson.toJson(sev);
+		//json = gson.toJson(sev);
 		
+		public void doGet(HttpServletRequest req, HttpServletResponse resp) 
+				throws IOException {
+			String json = gson.toJson(sev);
+			
 		//responder
-		resp.setContentType("text/json");
-		resp.getWriter().println(json);
+			resp.setContentType("text/json");
+			resp.getWriter().println(json);
 	}
 }
