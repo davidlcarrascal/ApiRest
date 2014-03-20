d9700f2ad5be541f88b2b2869aedbc92be3e6934
marbuziba <marbuziba@43ee67e5-c0c0-4f48-802a-26a3a79503e4>
2014-03-20 15:42:01 +0000

    Rocio pract POST

--- trunk/sos-2014-03/src/sos/us/es/RocioServelt.java
+++ trunk/sos-2014-03/src/sos/us/es/RocioServelt.java
@@ -1,4 +1,5 @@
 package sos.us.es;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -12,8 +13,8 @@ import com.google.gson.Gson;
 @SuppressWarnings("serial")
 public class RocioServelt extends HttpServlet {
 	
-	public static List<universitySeville> uni = new LinkedList<>();
-	public static Gson json = new Gson();
+//	public static List<universitySeville> uni = new LinkedList<>();
+//	public static Gson gson = new Gson();
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
@@ -22,6 +23,7 @@ public class RocioServelt extends HttpServlet {
 		Gson gson= new Gson();
 		
 		//crear datos de prueba: Crear lista 2 
+		List<universitySeville> uni = new LinkedList<>();
 		universitySeville u1 = new universitySeville (2010, 10000);
 		universitySeville u2 = new universitySeville (2011, 200000);
 		
@@ -38,17 +40,36 @@ public class RocioServelt extends HttpServlet {
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 		
-		resp.setContentType("text/json");
-		resp.getWriter().print(json);
+		universitySeville uni = null;
+		
+		Gson gson = new Gson();
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = req.getReader();
+		
+		String json;
+		
+		while((json = br.readLine())  != null){
+			sb.append(json);
+		}
+		json = sb.toString();
+		
+		try{
+			uni = gson.fromJson(json, universitySeville.class);
+		}catch(Exception e){
+			System.out.println("ERROR universitySevilla: "+e.getMessage());
+		}
+		
+	//	resp.setContentType("text/json");
+	//	resp.getWriter().print("---POST");
 	}
 	public void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 		
-		resp.setContentType("text/json");
-		resp.getWriter().print(json);
+		resp.setContentType("text/plain");
+		resp.getWriter().print("hola PUT");
 	}
 	public void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException{
-		uni.clear();
+		//uni.clear();
 		resp.setContentType("text/json");
-		resp.getWriter().println(json);
+		resp.getWriter().println("---DElete");
 	}
 }
