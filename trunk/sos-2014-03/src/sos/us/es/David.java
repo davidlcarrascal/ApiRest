b38032d32478d2e999441721c233f6465ee3fc70
davlopcar <davlopcar@43ee67e5-c0c0-4f48-802a-26a3a79503e4>
2014-03-20 15:42:42 +0000

    POST

--- trunk/sos-2014-03/src/sos/us/es/David.java
+++ trunk/sos-2014-03/src/sos/us/es/David.java
@@ -1,4 +1,5 @@
 package sos.us.es;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,15 +31,51 @@ public class David extends HttpServlet {
 		resp.getWriter().println(json);
 	}
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		//Crear un objeto estadistica( un objeto pueblo)
+		//sobre la lista da error
 		
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Estoy haciendo un POST");
+		//metemos datos con el ARC(advance rest client), estos datos eran JSON
+		//entonces una vez recibimos los datos lo cogemos y lo transformamos de JSON
+		//a objeto Java
+		
+		SpainStd spStad =null;
+		Gson gson = new Gson();
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = req.getReader();
+		
+		String jsonString;
+		
+		while((jsonString = br.readLine()) != null){
+			sb.append(jsonString);
+		}
+		
+		jsonString= sb.toString();
+		
+		try{
+			
+			spStad= gson.fromJson(jsonString, SpainStd.class);
+			
+		}catch(Exception e){
+			System.out.println("ERROR parsing SpainStd:" + e.getMessage());
+		}
+		
+		String json;
+		json=gson.toJson(spStad);
+
+		resp.setContentType("text/json");
+		resp.getWriter().println(json);
+		
+		
 	}
 	public void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		//put sobre la lista--> da error
+		//put sobre un objeto de la lista--> Actualiza
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Estoy haciendo un PUT");
 	}
 	public void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		//delete la lista
+		//delete un objeto
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Estoy haciendo un DELETE");
 	}
